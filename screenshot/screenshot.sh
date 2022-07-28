add80298de91b567500e91d6c5850866a73ec204
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-07-28 11:17:00 -0300

    Update screenshot.sh

--- screenshot/screenshot.sh
+++ screenshot/screenshot.sh
@@ -2,18 +2,11 @@
 if [[ ! -d ~/Imagens/screenshots/ ]]; then
 	mkdir -p ~/Imagens/screenshots/
 fi
-output=~/Imagens/screenshots/$(date +%d-%m-%y-%H-%M-%S).png
+print=~/Imagens/screenshots/$(date +%d-%m-%y-%H-%M-%S).png
 
 case "$1" in
-"select") maim -s "$output" || xclip -sel clip -t image/png "$output" || exit ;;
-*) maim "$output" || exit ;;
+"select") maim -s "$print" || xclip -sel clip -t image/png "$print" || exit ;;
+*) maim "$print" || exit ;;
 esac
 
-# Doesn't work in wayland, screenshot is a black screen
-# if [[ "$XDG_SESSION_TYPE" = "wayland" ]]; then
-# 	wl-copy <"$output"
-# else
-# 	xclip -sel clip -t image/png "$output"
-# fi
-
-notify-send "Screenshot saved in $output"
+notify-send "Screenshot saved in $print"
